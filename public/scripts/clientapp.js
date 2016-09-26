06b870bf4229761e1d2f3537e4da929bf6146f6c
Millie Walsh <millie@primeacademy.io>
2016-09-26 12:08:50 -0500

    add pet controller

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -18,3 +18,31 @@ myApp.controller('viewPetController', ['$scope', '$http',
       });
     };
 }]);
+
+myApp.controller('addPetController', ['$scope', '$http',
+function($scope, $http) {
+  console.log('add pet controller');
+  $scope.addPet = function(inputName, inputAnimal, inputAge, inputImage) {
+
+    console.log('button click');
+
+    var petToSend = {
+      name: inputName,
+      animal_type: inputAnimal,
+      age: inputAge,
+      image: inputImage
+    };
+
+    console.log('pet to send = ', petToSend);
+    
+    $http({
+      method: 'POST',
+      url: '/pets',
+      data: petToSend
+    }).then(function success(responseObject) {
+      console.log('got em=', responseObject);
+    }, function error(errorObject){
+      console.log(errorObject);
+    });
+  };
+}]);
