8e71cc5ef84e6cd5f67c5b43ebe64ec24802e2c1
Millie Walsh <millie@primeacademy.io>
2016-09-26 10:06:55 -0500

    wire up pets router

---
+++ routers/pets.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  console.log('in pets get');
+  res.sendStatus(200);
+});
+
+router.post('/', function(req, res) {
+  console.log('in pets post');
+  res.sendStatus(200);
+});
+
+module.exports = router;
